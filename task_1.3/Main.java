1c096d0949e286c02cfbf6533818b1c1562c8b3c
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-06 15:56:37 +0300

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,1 +1,9 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
 
+public class Main {
+    public static void main(String[] args) {
+        int a =5, b =7;
+        String s ="Сумма чисел: ";
+    }
+}
