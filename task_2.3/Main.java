12d171cb24093b3c7f4d8f1e5d1eaf68c9c0ef2a
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-09 16:14:48 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -16,7 +16,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j<i+1; j++){
+            for (int j = 0; j<=i; j++){
                 System.out.print("8");
             }
             System.out.println();
