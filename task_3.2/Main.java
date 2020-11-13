0777fc39f2c1f1026fb0c0799c87d1ff8c339c73
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-13 15:04:19 +0300

    Update Main.java

--- task_3.2/Main.java
+++ task_3.2/Main.java
@@ -1,13 +1,28 @@
+/*
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Функция min(a,b,c,d) должна использовать (вызывать) функцию min(a,b)
+Подсказка:
+Нужно написать тело двух существующих функций min.
+Требования:
+•  Программа должна выводить текст на экран.
+•  Методы min не должны выводить текст на экран.
+•  Метод main должен вызывать метод min(a, b) и метод min(a, b, c, d).
+•  Метод main должен выводить на экран результат работы методов min. Каждый раз с новой строки.
+•  Метод min(a, b) должен возвращать минимальное значение из чисел a, b.
+•  Метод min(a, b, c, d) должен использовать метод min(a, b)
+•  Метод min(a, b, c, d) должен возвращать минимальное значение из чисел a, b, c, d.
+*/
+
 public class Main {
     public static int min(int a, int b, int c, int d) {
-        if (a<=b && a<=c && a<=d)      return a;
-        else if (b<=a && b<=c && b<=d) return b;
-        else if (c<=a && c<=b && c<=d) return c;
+        if (min(a, b) && min(a, c) && min(a, d))      return a;
+        else if (min(b, a) && min(b, c) && min(b, d)) return b;
+        else if (min(c, a) && min(c, b) && min(c, d)) return c;
         else return d;
     }
 
     public static int min(int a, int b) {
-        if (a<b) return a;
+        if (a<=b) return a;
         else return b;
     }
 
