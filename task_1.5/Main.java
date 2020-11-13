e18566e12618e584b0369f96714d6a45537b0b04
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-13 14:53:57 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+        float a = scan.nextInt();
         System.out.println(a*9/5+32);
     }
 }
