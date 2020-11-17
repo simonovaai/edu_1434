63b171aeff47daac9390513325ff7d404d1dad7d
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-18 01:27:57 +0300

    Create Main.java

---
+++ task_5.3/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
+ * Коллекции вы создаёте сами
+ */
+
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(int i=0; i<=12; i++) {
+            if(i>0 && i<10) nums.add(i);
+        }
+        System.out.println(nums);
+    }
+}
