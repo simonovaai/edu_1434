076b42981b6d868009cf70c80f1b4fb87e9145f2
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-19 14:53:51 +0300

    Create Main.java

---
+++ task_5.4/Main.java
@@ -0,0 +1,48 @@
+/*
+1. Ввести путь к файлу с консоли.
+2. Прочитать из него набор чисел.
+3. Вывести на консоль только четные, отсортированные по возрастанию.
+Пример ввода:
+5
+8
+-2
+11
+3
+-5
+2
+10
+Пример вывода:
+-2
+2
+8
+10
+*/
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //открываем входящий поток, читающий с консоли
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        //поток, читающий из файла
+        reader = new BufferedReader(new InputStreamReader((new FileInputStream(fileName))));
+        //Коллекция
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        // Преобразуем в int и узнаем четный или нет
+        while (reader.ready()) {
+            int num = Integer.parseInt(reader.readLine());
+            if (num % 2 == 0) {
+                numbers.add(num);
+            }
+        }
+        // Закрываем ридер
+        reader.close();
+        // Сортируем коллекцию
+        Collections.sort(numbers);
+        // вывод на экран
+        for (int i=0; i<numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
+}
