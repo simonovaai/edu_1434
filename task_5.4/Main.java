b5a3cedd385a130fd45af348feb2515230bef152
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-19 14:58:29 +0300

    Update Main.java

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -22,25 +22,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class Main {
     public static void main(String[] args) throws IOException {
-        //открываем входящий поток, читающий с консоли
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-        //поток, читающий из файла
         reader = new BufferedReader(new InputStreamReader((new FileInputStream(fileName))));
-        //Коллекция
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        // Преобразуем в int и узнаем четный или нет
         while (reader.ready()) {
             int num = Integer.parseInt(reader.readLine());
             if (num % 2 == 0) {
                 numbers.add(num);
             }
         }
-        // Закрываем ридер
         reader.close();
-        // Сортируем коллекцию
         Collections.sort(numbers);
-        // вывод на экран
         for (int i=0; i<numbers.size(); i++) {
             System.out.println(numbers.get(i));
         }
