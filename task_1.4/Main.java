14658f9c2c29b867fa2ccdc178892e2ee91605e3
simonovaai <73995604+simonovaai@users.noreply.github.com>
2020-11-30 15:15:02 +0300

    Delete Main.java

--- task_1.4/Main.java
+++
@@ -1,9 +0,0 @@
-/* Дана переменная number.
-Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
-
-public class Main {
-    public static void main(String[] args) {
-        int number = 25;
-        System.out.print(number * number);
-    }
-}
